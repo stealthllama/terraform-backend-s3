64ff8a7c1eefaa65beaee65f984a9001ff0e6a85
Robert Hagen <stealthllama@users.noreply.github.com>
2019-06-11 10:23:57 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,59 @@
+############################################################################################
+# Copyright 2019 Palo Alto Networks.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+############################################################################################
+
+provider "aws" {
+    access_key = "${var.aws_access_key_id}"
+    secret_key = "${var.aws_secret_access_key}"
+    region = "${var.aws_region}"
+    version = "~> 2.14"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 8
+}
+
+resource "aws_s3_bucket" "terraform-state-bucket" {
+    bucket = "terraform-state-bucket-${random_id.suffix.hex}"
+ 
+    versioning {
+      enabled = true
+    }
+ 
+    lifecycle {
+      prevent_destroy = true
+    }
+ 
+    tags {
+      Name = "S3 Remote Terraform State Store"
+    }      
+}
+
+resource "aws_dynamodb_table" "terraform-lock-dynamodb" {
+  name = "terraform-state-lock-${random_id.suffix.hex}"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+ 
+  tags {
+    Name = "DynamoDB Terraform State Lock Table"
+  }
+
+}
